b98fc61652221f6eb8408db650866fdb571ca8e0
Ismailium <qiranorane55@gmail.com>
2024-01-14 18:09:33 +0100

    Added the data transform objects (DTOs) for create/update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,11 +4,15 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users') // (routes)
 export class UsersController {
@@ -21,40 +25,32 @@ export class UsersController {
   }
 
   @Get(':id') // GET => /users/:id
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     // (+) unary
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST => /users
   create(
-    @Body()
-    user: {
-      name: string;
-      email: string;
-      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Body(ValidationPipe)
+    user: CreateUserDto,
   ) {
     return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH/PUT/UPDATE => /users/:id
   update(
-    @Param('id') id: number,
-    @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe)
+    user: UpdateUserDto,
   ) {
     // (+) unary
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, user);
   }
 
   @Delete(':id') // DELETE => /users/:id
-  delete(@Param('id') id: number) {
+  delete(@Param('id', ParseIntPipe) id: number) {
     // (+) unary
-    return this.usersService.delete(+id);
+    return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -45,11 +47,7 @@ export class UsersService {
   }
 
   // Create user
-  create(user: {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-  }) {
+  create(user: CreateUserDto) {
     const highestId = [...this.users].sort((x, y) => y.id - x.id);
     const newUser = {
       id: highestId[0].id + 1, // Generating the next highest id
@@ -61,14 +59,7 @@ export class UsersService {
   }
 
   // Update user
-  update(
-    id: number,
-    updatedUser: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
-  ) {
+  update(id: number, updatedUser: UpdateUserDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
         return { ...user, ...updatedUser };
